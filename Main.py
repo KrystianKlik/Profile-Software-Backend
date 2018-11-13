fa49478b4df0e9e98efca7c90c3bbd10c798a06e
Krystian Klik <krystianklik@gmail.com>
2018-11-13 19:58:58 +0100

    Added cleaning Data

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import argparse
 
 class Prompt:
-
     def __init__(self, name):
         self.name = name
 
@@ -34,38 +33,61 @@ class Text(Prompt):
 
     def counting_scores(self):
 
-
         self.scrabble_scores()
 
         with open(args.source + ".txt", 'r') as file:
             data = file.readlines()
 
+        listOfWords = []
+        # Cleaning data
         for line in data:
-            words = line.split()
+            # Deleting all whitespaces in word
+            cleanLine = line.rstrip()
+            if cleanLine is not '':
+                # Deleting tabulation
+                if '\t' in cleanLine:
+                    cleanLine = cleanLine.replace('\t', '')
+
+                listOfWords.append(cleanLine)
+                lengthListOfWords = len(listOfWords)
+                print(listOfWords)
 
         arrayOfScore = []
         total_score = 0
 
-        for line in range(len(words)):
-            for word in range(len(words[line])):
+        for line in range(lengthListOfWords):
+            for word in range(len(listOfWords[line])):
                 for letter, score in self.LETTER_SCORES.items():
-                    if words[line][word].upper() == letter:
+                    if listOfWords[line][word].upper() == letter:
                         total_score += score
+
             arrayOfScore.append(total_score)
             total_score = 0
 
-        dictionary = dict(zip(words, arrayOfScore))
+        dictionary = dict(zip(listOfWords, arrayOfScore))
 
-        indexOfBiggestValue = arrayOfScore.index(max(arrayOfScore))
-        print(f'The biggest value is: {words[indexOfBiggestValue]}')
-        print(f'This is to check values: {dictionary}')
+        print(dictionary)
+
+        return dictionary
+
+    def biggestValue(self, d):
+        """ a) create a list of the dict's keys and values;
+            b) return the key with the max value"""
+        v=list(d.values())
+        k=list(d.keys())
+        word = k[v.index(max(v))]
+        print(f'The biggest value is {word} with {max(v)}  points')
+
+    def findPoints(directory, counting_scores, points):
+        print (counting_scores)
+
+
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('type', help="If it's a file with extension .txt write [txt] on prompt write word [prompt]")
 parser.add_argument('source', help="Type text in prompt or name of .txt file, note to make it easier file must be in the same folder as script")
 args = parser.parse_args()
-
 
 name = args.source
 
@@ -77,7 +99,9 @@ if args.type == "prompt":
 elif args.type == "txt":
     print(Text(name))
     a = Text(name)
-    a.counting_scores()
+    p = a.counting_scores()
+    # a.findPoints(p, 10)
+    a.biggestValue(p)
 
 else:
     print("Wrong value if you don't know how to handle this check --help")
